a16be3d6c79bc021b5cf310d5d7f8253d3656969
Rusydina Novitasari <rnvitas697@gmail.com>
2024-09-13 23:18:48 +0700

    fitur login dan regist v-2 pake localstorage

--- js/login.js
+++ js/login.js
@@ -1,39 +1,15 @@
-let users = [
-  {
-    id: 1,
-    email: "rnvitas697@gmail.com",
-    password: "12345678",
-    status: 1,
-    active: 1,
-  },
-  {
-    id: 2,
-    email: "resaarfita@gmail.com",
-    password: "1234567",
-    status: 1,
-    active: 1,
-  },
-  {
-    id: 3,
-    email: "resaarfita@gmail.com",
-    password: "1234567",
-    status: 1,
-    active: 1,
-  },
-];
-
-const api = "https://66dfecb22fb67ac16f279532.mockapi.io/Shesafe/users";
-
-
-async function login() {
+function login() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
+  let users = JSON.parse(localStorage.getItem("users")) || [];
+
+  if (!email || !password) {
+    document.getElementById("error-message").innerHTML =
+      "Email and password cannot be empty.";
+    return;
+  }
 
   try {
-    //kalo pake api
-    let response = await fetch(api);
-    let users = await response.json();
-
     const user = users.find(
       (user) =>
         user.email === email &&
@@ -43,7 +19,6 @@ async function login() {
     );
     if (user) {
       localStorage.setItem("LoggedIn", "true");
-      localStorage.setItem("email", email);
 
       window.location.href = "home.html";
     } else {
